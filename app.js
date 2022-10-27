24b0d1b977cca75199a480818157c13840d4ff5d
nina <ninaburaschi@gmail.com>
2022-10-27 12:22:04 -0300

    subiendo todo

---
+++ app.js
@@ -0,0 +1,44 @@
+const formulario = document.getElementById("formulario"); //captur los elementos
+
+const userName = document.getElementById("userName"); 
+const userEmail = document.getElementById("userEmail");
+const userConsulta = document.getElementById('userConsulta');
+
+const alertSuccess = document.getElementById("alertSuccess");
+const alertName = document.getElementById("alertName");
+const alertEmail = document.getElementById("alertEmail");
+
+const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+const pintarMensajeExito = () => {
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "Mensaje enviado con éxito";
+};
+
+const pintarMensajeError = (errores) => {
+    errores.forEach((item) => {
+        item.tipo.classList.remove("d-none");
+        item.tipo.textContent = item.msg;
+    });
+};
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alertSuccess.classList.add("d-none");
+    const errores = [];
+
+    // validar nombre
+    if (!regUserName.test(userName.value) || !userName.value.trim()) {
+        userName.classList.add("is-invalid");
+
+        errores.push({
+            tipo: alertName,
+            msg: "Formato no válido, ingrese solo letras",
+        });
+    } else {
+        userName.classList.remove("is-invalid");
+        userName.classList.add("is-valid");
+        alertName.classList.add("d-none");
+    }
